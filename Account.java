c5826f0cde88dc1a8db621e5382a60c12e1e9031
cdesmang <desmangc@email.sc.edu>
2022-03-15 14:24:21 -0400

    added some stubs

--- Account.java
+++ Account.java
@@ -1,3 +1,25 @@
+import java.util.*;
 public class Account {
+    private String fullName;
+    private String userName;
+    private String password;
+    private String email;
+    private Date dob;
+    private String address;
+    private ArrayList <FilterOptions> filters;
+
+    public Account(String fullName, 
+                   String username, 
+                   String password, 
+                   String email, 
+                   Date dob, 
+                   String address) {}
+
+    public void createAccount (){}
+
+    public void deleteAccount (String username, 
+                               String password){}
+
+    public void setPreferences() {}
     
 }
