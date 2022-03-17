0046891dd1309de3ac467e7abe04a4d17293d099
cdesmang <desmangc@email.sc.edu>
2022-03-17 14:30:47 -0400

    added ID

--- Flight.java
+++ Flight.java
@@ -5,6 +5,7 @@ public class Flight {
     private String departLoc;
     private int numTickets;
     private String flightType;
+    public String ID;
 
     public Flight (String departTime, String arrTime, String departLoc,int Tickets, String fType) {
 
