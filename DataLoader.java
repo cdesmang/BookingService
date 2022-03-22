c556b1ee72f1f84f1b0b3f3d897029009740fe9f
Max Strickland <mcs30@email.sc.edu>
2022-03-22 13:41:34 -0400

    DataLoader TESTER ONLY

--- DataLoader.java
+++ DataLoader.java
@@ -2,6 +2,61 @@
  * Loads information from JSON files
  */
 
+//This is only a tester.
+import java.util.Scanner;	//used for inputting the JSON file
+import java.io.FileReader;	//used to read files
+import java.util.ArrayList; //used for arraylists
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;;
+
+public class DataLoader {
+	public static void main(String[] args)
+	{
+		//These lines of code try to read the entire file.
+		try 
+		{
+			FileReader fileReader = new FileReader("src\\\\flights.json"); //needs filepath
+			JSONParser fileParser = new JSONParser();
+			JSONArray myJSONArray = (JSONArray) new JSONParser().parse(fileReader);
+			
+			//
+			for(int i = 0; i < myJSONArray.size(); i++)
+			{
+				JSONObject tempJSONObject = (JSONObject) myJSONArray.get(i);
+				String id = (String)tempJSONObject.get("id");
+				long flightNumber = (long)tempJSONObject.get("flightNumber");
+				String airline = (String)tempJSONObject.get("airline");
+				String departureTime = (String)tempJSONObject.get("departureTime");
+				String arrivalTime = (String)tempJSONObject.get("arrivalTime");
+				String flightTime = (String)tempJSONObject.get("flightDuration");
+				
+				//This thing doesn't always exist, but you can't get a seat which doesn't exist.
+				boolean SixJ = false;
+				if(tempJSONObject.get("6J") != null)
+					SixJ = (boolean)tempJSONObject.get("6J");
+				
+				System.out.println("Welcome to flight " + flightNumber + " with airline " + airline + " and flight id " + id + ".");
+				System.out.println("This flight leaves at " + departureTime + " and arrives at " + arrivalTime + " , for a total flight time of " + flightTime + ".");
+				System.out.println("Seat 6J is available: " + SixJ);
+			}
+		} 
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return;
+	}
+}
+
+/*
+ * This is version 1.0, uploaded on 2022 Mar 22.
+ * This merely runs some tests using standard output; it is not made to supplant the final code.
+ * Future updates will be concern with separating this code into methods.
+*/
+
+/*
+ * This is commented out temporarily while the tester is being ran.
 import java.util.*;
 public class DataLoader {
 
@@ -17,3 +72,4 @@ public class DataLoader {
 
     // should we also have one for hotel ???
 } 
+*/
