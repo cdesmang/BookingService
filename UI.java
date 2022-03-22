53fa0d91399192c75a097d28f2fea56c5ef5060e
cdesmang <desmangc@email.sc.edu>
2022-03-22 13:32:56 -0400

    added welcome statement

--- UI.java
+++ UI.java
@@ -1,3 +1,14 @@
+/**
+ * User Interface 
+ * @author Christina Desmangles
+ */
 public class UI {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Flight Booking Service!");
+        
+
+    }
     
 }
