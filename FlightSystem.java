027e2c37a02c95e39f1e15714d4519fa42ca0ad4
cdesmang <desmangc@email.sc.edu>
2022-03-24 14:24:43 -0400

    added some methods

--- FlightSystem.java
+++ FlightSystem.java
@@ -8,13 +8,20 @@ public class FlightSystem {
     public FlightSystem() {
         
     }
-    public ArrayList<Flight> getAllFlights () {
-        ArrayList<Flight> temp) = Flights.getInstance();
+    public ArrayList<Flight> getAllFlights (String dLoc, String aLoc, Date dDate, Date aDate) {
+        ArrayList<Flight> temp = flights.getInstance();
+        return temp;
+    }
+
+    public ArrayList<Flight>searchFlights (ArrayList<Flight> broad){
+        ArrayList<Flight> temp = new ArrayList<Flight>();
+        return temp;
     }
 
     public User getCurrentUser (){
         return currentUser;
     }
+
 
     public boolean login(){
         return true;
--- UI.java
+++ UI.java
@@ -3,14 +3,87 @@
  * User Interface 
  * @author Christina Desmangles
  */
+
 import java.util.Scanner;
 import java.util.ArrayList;
 public class UI {
 
-
     private static final Scanner key = new Scanner(System.in);
-    private FlightSystem flightSystem = new FlightSystem();
+    private static FlightSystem flightSystem = new FlightSystem();
+
     public static void main(String[] args) {
+
+        boolean run = true;
+        while (run) {
+            String login= Welcome();
+            if (login == "GUEST") {
+                guestSearch();
+            }
+            else if (login == "SIGN IN"){
+                registeredSearch();
+            }
+            else{
+                System.out.println("Invalid input!");
+                run = true;
+            }
+        }
+    }
+
+    public static String Welcome(){ 
+        System.out.println("Welcome to the Flight Booking System!"+
+        "\nWould you like to continue searching as a guest, or would you like to sign in?"+
+        "\nTo continue as a guest please enter \"guest\" or to sign in please enter \"sign in\"");
+        String login = key.nextLine().toUpperCase();
+        return login;
+    }
+
+    public static void guestSearch(){
+         getFlights();
+
+    }
+
+    public static void registeredSearch(){
+
+    }
+
+   /* DO THIS INSIDE OF FLIGHT SYSTEM
+    private static Date getDate(String date){
+        String temp = date;
+        String[] sections = temp.split("/");
+        int month = Integer.parseInt(sections[0]);
+        int day = Integer.parseInt(sections[1]);
+        int year = Integer.parseInt(sections[2]);
+        Date created = new Date(month,day,year);
+    }*/
+
+    private static Location getLocation(String loc) {
+
+    }
+
+    private static ArrayList<Flights> getFlights(){
+        System.out.println("Pleae enter your departure location in the format \"city, State\"");
+        String dLoc= key.nextLine();
+        System.out.println("Please enter your departure date in the format\"MM/DD/YYYY\"");
+        String dDate= key.nextLine();
+        System.out.println("Pleae enter your arrival location in the format \"city, State\"");
+        String aLoc= key.nextLine();
+        System.out.println("Please enter your arrival date in the format\"MM/DD/YYYY\"");
+        String aDate= key.nextLine();
+        flightSystem.getAllFlights(dLoc, aLoc, dDate, aDate);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+   /* public static void main(String[] args) {
         //Setting up the scanner...
         Scanner inputReader = new Scanner(System.in);   //This initializes a Scanner object.
         String input = "";  //This string will be updated to reflect user input.
@@ -49,11 +122,11 @@ public class UI {
 
     }
 
-    private ArrayList<Flights> getFlights(){
-        Flight
+    private ArrayList<Flights> getFlights){
+        return flightSystem.getAllFlights();
     }
 
-   // private static 
+   // private static */
 
     
 }
